7c8a185d9893305edcc3a79df3917e60017d989e
Mehryarr <mehryarja@gmail.com>
2024-06-19 23:18:33 +0330

    6/19/2024

--- awesomefuncs/main.go
+++ awesomefuncs/main.go
@@ -1,9 +1,16 @@
 package main
 
+import (
+	"strconv"
+)
+
 type FilterFunc func(int) bool
 type MapperFunc func(int) int
 
 func IsSquare(x int) bool {
+	if x == 0 {
+		return true
+	}
 	for i := 1; ; i = i + 2 {
 		x -= i
 		if x == 0 {
@@ -15,26 +22,46 @@ func IsSquare(x int) bool {
 }
 
 func IsPalindrome(x int) bool {
-	//TODO
-	return false
+
+	y := strconv.Itoa(x)
+
+	if x < 0 {
+		y = y[1:]
+	}
+
+	for i := 0; i < len(y)/2; i++ {
+		if y[i] != y[len(y)-i-1] {
+			return false
+		}
+	}
+	return true
 }
 
 func Abs(num int) int {
-	//TODO
-	return 0
+	if num < 0 {
+		return -1 * num
+	}
+	return num
 }
 
 func Cube(num int) int {
-	//TODO
-	return 0
+	return num * num * num
 }
 
 func Filter(input []int, f FilterFunc) []int {
-	//TODO
-	return nil
+	res := make([]int, 0, len(input))
+	for _, v := range input {
+		if f(v) {
+			res = append(res, v)
+		}
+	}
+	return res
 }
 
 func Map(input []int, m MapperFunc) []int {
-	//TODO
-	return nil
+	var res []int
+	for _, v := range input {
+		res = append(res, m(v))
+	}
+	return res
 }
