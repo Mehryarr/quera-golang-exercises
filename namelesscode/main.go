1537d9aeed29101554f875e258a9cac51c3ef726
Mehryarr <mehryarja@gmail.com>
2024-06-21 17:46:34 +0330

    6/21/2024

--- namelesscode/main.go
+++ namelesscode/main.go
@@ -1,18 +1,37 @@
 package main
 
 func AddElement(numbers *[]int, element int) {
-	//TODO
+	*numbers = append(*numbers, element)
 }
 
 func FindMin(numbers *[]int) int {
-	//TODO
-	return 0
+
+	if *numbers == nil || len(*numbers) == 0 {
+		return 0
+	}
+
+	min := (*numbers)[0]
+	for _, v := range *numbers {
+		if v < min {
+			min = v
+		}
+	}
+	return min
+
 }
 
 func ReverseSlice(numbers *[]int) {
-	//TODO
+	ReverseNums := make([]int, 0)
+
+	for i := len(*numbers) - 1; i >= 0; i-- {
+		ReverseNums = append(ReverseNums, (*numbers)[i])
+	}
+	copy(*numbers, ReverseNums)
 }
 
 func SwapElements(numbers *[]int, i, j int) {
-	//TODO
+	if numbers == nil || len(*numbers) == 0 || i < 0 || j < 0 || i >= len(*numbers) || j >= len(*numbers) {
+		return
+	}
+	(*numbers)[i], (*numbers)[j] = (*numbers)[j], (*numbers)[i]
 }
