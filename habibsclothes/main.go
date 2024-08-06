e8c8f46a3218f2599c0cd850dffc69634e3be6f2
Mehryar <mehryarja@gmail.com>
2024-08-06 22:55:10 +0330

    editing the loops

--- habibsclothes/main.go
+++ habibsclothes/main.go
@@ -25,46 +25,91 @@ func main() {
 
 		switch {
 		case firstfield == "COAT:":
-			for i := 0; i < len(parts)-1; i++ {
-				clothes.coat = append(clothes.coat, parts[i+1])
-			}
+			clothes.coat = append(clothes.coat, parts[1:]...)
+
 		case firstfield == "SHIRT:":
-			for i := 0; i < len(parts)-1; i++ {
-				clothes.coat = append(clothes.coat, parts[i+1])
-			}
+			clothes.shirt = append(clothes.shirt, parts[1:]...)
+
 		case firstfield == "PANTS:":
-			for i := 0; i < len(parts)-1; i++ {
-				clothes.coat = append(clothes.coat, parts[i+1])
-			}
+			clothes.pants = append(clothes.pants, parts[1:]...)
+
 		case firstfield == "CAP:":
-			for i := 0; i < len(parts)-1; i++ {
-				clothes.coat = append(clothes.coat, parts[i+1])
-			}
+			clothes.cap = append(clothes.cap, parts[1:]...)
+
 		case firstfield == "JACKET:":
-			for i := 0; i < len(parts)-1; i++ {
-				clothes.coat = append(clothes.coat, parts[i+1])
-			}
-		default:
-			break
+			clothes.jacket = append(clothes.jacket, parts[1:]...)
 		}
 	}
+
+	clothes.coat = append(clothes.coat, "0")
+	clothes.jacket = append(clothes.jacket, "5")
 	myscanner.Scan()
 	Season := myscanner.Text()
 	switch Season {
 	case "SPRING":
-		fmt.Println("spring")
-	case "SUMMER":
-		fmt.Println("summer")
-	case "FALL":
-		fmt.Println("fall")
-	case "WINTER":
-		fmt.Println("winter")
-	}
+		for _, i := range clothes.shirt {
+			for _, j := range clothes.pants {
+				if len(clothes.cap) > 0 {
+					for _, k := range clothes.cap {
+						if len(clothes.coat) > 0 {
+							for _, q := range clothes.coat {
+								if q != "0" {
+									fmt.Printf("COAT: %s SHIRT: %s PANTS: %s CAP: %s\n", q, i, j, k)
+								}
+							}
+						} else {
+							fmt.Printf("SHIRT: %s PANTS: %s CAP: %s\n", i, j, k)
+						}
+					}
+				} else {
+					fmt.Printf("SHIRT: %s PANTS: %s\n", i, j)
+				}
 
-	fmt.Println(clothes.coat)
-	fmt.Println(clothes.shirt)
-	fmt.Println(clothes.pants)
-	fmt.Println(clothes.cap)
-	fmt.Println(clothes.jacket)
+			}
+		}
+
+	case "SUMMER":
+		for i := 0; i < len(clothes.shirt); i++ {
+			for j := 0; j < len(clothes.pants); j++ {
+				for k := 0; k < len(clothes.cap); k++ {
+					fmt.Printf("SHIRT: %s PANTS: %s CAP: %s\n", clothes.shirt[i], clothes.pants[j], clothes.cap[k])
+				}
+			}
+		}
+
+	case "FALL":
+		for _, i := range clothes.shirt {
+			for _, j := range clothes.pants {
+				for _, k := range clothes.cap {
+					if len(clothes.coat) > 0 {
+						for _, q := range clothes.coat {
+							if q != "0" && q != "orange" && q != "yellow" {
+								fmt.Printf("COAT: %s SHIRT: %s PANTS: %s CAP: %s\n", q, i, j, k)
+							}
+						}
+					} else {
+						fmt.Printf("SHIRT: %s PANTS: %s CAP: %s\n", i, j, k)
+					}
+				}
+			}
+		}
+
+	case "WINTER":
+		for i := 0; i < len(clothes.coat); i++ {
+			for j := 0; j < len(clothes.shirt); j++ {
+				for k := 0; k < len(clothes.pants); k++ {
+					for q := 0; q < len(clothes.jacket); q++ {
+						if clothes.coat[i] == "0" && clothes.jacket[q] != "5" {
+							fmt.Printf("SHIRT: %s PANTS: %s JACKET: %s\n", clothes.shirt[j], clothes.pants[k], clothes.jacket[q])
+						} else if clothes.jacket[q] == "5" && clothes.coat[i] != "0" {
+							fmt.Printf("COAT: %s SHIRT: %s PANTS: %s\n", clothes.coat[i], clothes.shirt[j], clothes.pants[k])
+						}
+					}
+
+				}
+			}
+		}
+
+	}
 
 }
