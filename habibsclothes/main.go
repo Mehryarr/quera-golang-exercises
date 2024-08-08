8212087516a858cb3bd56d5d1f83f280c8ec6bf1
Mehryarr <mehryarja@gmail.com>
2024-08-08 13:04:03 +0330

    8/8/2024

--- habibsclothes/main.go
+++ habibsclothes/main.go
@@ -41,75 +41,96 @@ func main() {
 		}
 	}
 
-	clothes.coat = append(clothes.coat, "0")
-	clothes.jacket = append(clothes.jacket, "5")
 	myscanner.Scan()
 	Season := myscanner.Text()
 	switch Season {
 	case "SPRING":
-		for _, i := range clothes.shirt {
-			for _, j := range clothes.pants {
-				if len(clothes.cap) > 0 {
-					for _, k := range clothes.cap {
-						if len(clothes.coat) > 0 {
-							for _, q := range clothes.coat {
-								if q != "0" {
-									fmt.Printf("COAT: %s SHIRT: %s PANTS: %s CAP: %s\n", q, i, j, k)
-								}
-							}
-						} else {
-							fmt.Printf("SHIRT: %s PANTS: %s CAP: %s\n", i, j, k)
-						}
-					}
-				} else {
-					fmt.Printf("SHIRT: %s PANTS: %s\n", i, j)
+		for _, coat := range clothes.coat {
+			for _, shirt := range clothes.shirt {
+				for _, pants := range clothes.pants {
+					fmt.Printf("COAT: %s SHIRT: %s PANTS: %s\n", coat, shirt, pants)
 				}
-
+			}
+		}
+		for _, shirt := range clothes.shirt {
+			for _, pants := range clothes.pants {
+				for _, caps := range clothes.cap {
+					fmt.Printf("SHIRT: %s PANTS: %s CAP: %s\n", shirt, pants, caps)
+				}
+			}
+		}
+		for _, coat := range clothes.coat {
+			for _, shirt := range clothes.shirt {
+				for _, pants := range clothes.pants {
+					for _, caps := range clothes.cap {
+						fmt.Printf("COAT: %s SHIRT: %s PANTS: %s CAP: %s\n", coat, shirt, pants, caps)
+					}
+				}
+			}
+		}
+		for _, shirt := range clothes.shirt {
+			for _, pants := range clothes.pants {
+				fmt.Printf("SHIRT: %s PANTS: %s\n", shirt, pants)
 			}
 		}
 
 	case "SUMMER":
-		for i := 0; i < len(clothes.shirt); i++ {
-			for j := 0; j < len(clothes.pants); j++ {
-				for k := 0; k < len(clothes.cap); k++ {
-					fmt.Printf("SHIRT: %s PANTS: %s CAP: %s\n", clothes.shirt[i], clothes.pants[j], clothes.cap[k])
+		for _, shirt := range clothes.shirt {
+			for _, pants := range clothes.pants {
+				for _, caps := range clothes.cap {
+					fmt.Printf("SHIRT: %s PANTS: %s CAP: %s\n", shirt, pants, caps)
 				}
 			}
 		}
 
 	case "FALL":
-		for _, i := range clothes.shirt {
-			for _, j := range clothes.pants {
-				for _, k := range clothes.cap {
-					if len(clothes.coat) > 0 {
-						for _, q := range clothes.coat {
-							if q != "0" && q != "orange" && q != "yellow" {
-								fmt.Printf("COAT: %s SHIRT: %s PANTS: %s CAP: %s\n", q, i, j, k)
-							}
-						}
-					} else {
-						fmt.Printf("SHIRT: %s PANTS: %s CAP: %s\n", i, j, k)
+		for _, coat := range clothes.coat {
+			if coat != "orange" && coat != "yellow" {
+				for _, shirt := range clothes.shirt {
+					for _, pants := range clothes.pants {
+						fmt.Printf("COAT: %s SHIRT: %s PANTS: %s\n", coat, shirt, pants)
 					}
 				}
 			}
 		}
-
+		for _, shirt := range clothes.shirt {
+			for _, pants := range clothes.pants {
+				for _, caps := range clothes.cap {
+					fmt.Printf("SHIRT: %s PANTS: %s CAP: %s\n", shirt, pants, caps)
+				}
+			}
+		}
+		for _, coat := range clothes.coat {
+			if coat != "orange" && coat != "yellow" {
+				for _, shirt := range clothes.shirt {
+					for _, pants := range clothes.pants {
+						for _, caps := range clothes.cap {
+							fmt.Printf("COAT: %s SHIRT: %s PANTS: %s CAP: %s\n", coat, shirt, pants, caps)
+						}
+					}
+				}
+			}
+		}
+		for _, shirt := range clothes.shirt {
+			for _, pants := range clothes.pants {
+				fmt.Printf("SHIRT: %s PANTS: %s\n", shirt, pants)
+			}
+		}
 	case "WINTER":
-		for i := 0; i < len(clothes.coat); i++ {
-			for j := 0; j < len(clothes.shirt); j++ {
-				for k := 0; k < len(clothes.pants); k++ {
-					for q := 0; q < len(clothes.jacket); q++ {
-						if clothes.coat[i] == "0" && clothes.jacket[q] != "5" {
-							fmt.Printf("SHIRT: %s PANTS: %s JACKET: %s\n", clothes.shirt[j], clothes.pants[k], clothes.jacket[q])
-						} else if clothes.jacket[q] == "5" && clothes.coat[i] != "0" {
-							fmt.Printf("COAT: %s SHIRT: %s PANTS: %s\n", clothes.coat[i], clothes.shirt[j], clothes.pants[k])
-						}
-					}
-
+		for _, shirt := range clothes.shirt {
+			for _, pants := range clothes.pants {
+				for _, jacket := range clothes.jacket {
+					fmt.Printf("SHIRT: %s PANTS: %s JACKET: %s\n", shirt, pants, jacket)
 				}
 			}
 		}
-
+		for _, coat := range clothes.coat {
+			for _, shirt := range clothes.shirt {
+				for _, pants := range clothes.pants {
+					fmt.Printf("COAT: %s SHIRT: %s PANTS: %s\n", coat, shirt, pants)
+				}
+			}
+		}
 	}
 
 }
